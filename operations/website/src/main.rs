12aadb50b9168ea769d36430286d5f4b6e27b9f4
Joakim Soderlund <joakim.soderlund@gmail.com>
2025-06-05 11:04:06 +0200

    Redirect requrests to canonical host
    This may be better handled in the reverse proxy. It is difficult to know
    where we are currently running. Is this a development server? Can the
    development server handle HTTPS? What if we want to run the development
    server in release mode? Should we duplicate the canonicalize function
    across multiple services? What if someone else is hosting files? This
    may become overly complicated.

--- operations/website/src/main.rs
+++ operations/website/src/main.rs
@@ -1,12 +1,31 @@
 use axum::Router;
+use axum::extract::Request;
+use axum::middleware;
+use axum::middleware::Next;
+use axum::response::IntoResponse;
 use axum::response::Redirect;
+use axum::response::Response;
+use axum_extra::extract::Host;
 use std::env::var;
 use std::io::Result;
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 const BIND: &str = "[::]:34407";
+const HOST: &str = "www.fimfarchive.net";
 const PROFILE: &str = "https://www.fimfiction.net/user/116950/Fimfarchive";
+
+async fn canonicalize(Host(host): Host, req: Request, next: Next) -> Response {
+    let canon = var("HOST").unwrap_or(HOST.into());
+
+    if host == canon {
+        next.run(req).await
+    } else {
+        let path = req.uri().path_and_query().unwrap();
+        let url = format!("https://{canon}{path}");
+        Redirect::to(&url).into_response()
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -15,7 +34,8 @@ async fn main() -> Result<()> {
 
     let routes = Router::new()
         .nest_service("/releases", ServeDir::new("releases"))
-        .fallback(async || Redirect::to(PROFILE));
+        .fallback(async || Redirect::to(PROFILE))
+        .layer(middleware::from_fn(canonicalize));
 
     axum::serve(listener, routes).await
 }
