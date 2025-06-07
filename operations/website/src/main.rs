256a6552e9c37c11e68b5dc197fb5de150f234e7
Joakim Soderlund <joakim.soderlund@gmail.com>
2025-06-07 17:59:53 +0200

    Increase buffer size for website downloads

--- operations/website/src/main.rs
+++ operations/website/src/main.rs
@@ -6,7 +6,12 @@ use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 const BIND: &str = "[::]:34407";
+const CHUNK: usize = 16_777_216;
 const PROFILE: &str = "https://www.fimfiction.net/user/116950/Fimfarchive";
+
+fn releases() -> ServeDir {
+    ServeDir::new("releases").with_buf_chunk_size(CHUNK)
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -14,7 +19,7 @@ async fn main() -> Result<()> {
     let listener = TcpListener::bind(address).await?;
 
     let routes = Router::new()
-        .nest_service("/releases", ServeDir::new("releases"))
+        .nest_service("/releases", releases())
         .fallback(async || Redirect::to(PROFILE));
 
     axum::serve(listener, routes).await
